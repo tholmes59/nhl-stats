707604e0aceee2094975a72480f3eaab23429497
Thomas <tholmes59@gmail.com>
2020-01-15 14:28:52 -0800

    create reducer for teams

---
+++ src/reducers/teams.js
@@ -0,0 +1,8 @@
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'LIST_TEAMS':
+            return action.teams
+        default:
+            return state
+    }
+}
