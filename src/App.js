0533dec0a11cc445e2214d2feb42a38586559f11
Thomas <tholmes59@gmail.com>
2020-01-17 08:45:56 -0800

    add player component and route to app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { fetchTeams } from './actions/teams.js';
 import { connect } from 'react-redux';
 import Teams from './components/Teams'
 import Roster from './components/Roster'
+import Player from './components/Player'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
@@ -20,6 +21,7 @@ class App extends React.Component {
         <Router>
           <Route exact path="/" render={props => <Teams key={this.props.teams && this.props.teams.id} teams={this.props.teams}/>} />
           <Route exact path="/roster" render={props => <Roster key={this.props.roster && this.props.roster.id} roster={this.props.roster} teams={this.props.teams}/>} />
+          <Route path="/player" component={Player} />
         </Router>
         {/* <Teams tteams={this.props.teams}/> */}
       </div>
