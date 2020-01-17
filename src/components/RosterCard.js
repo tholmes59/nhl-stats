2bcb7d8674f3728a4797e7cf8153f99ff08fdb4b
Thomas <tholmes59@gmail.com>
2020-01-17 08:39:53 -0800

    add handleclick events to button click

--- src/components/RosterCard.js
+++ src/components/RosterCard.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { fetchPlayer } from '../actions/player'
+import { fetchPlayerStats } from '../actions/player'
+import { Redirect } from 'react-router-dom';
 
 class RosterCard extends React.Component {
+
+    handleClick = event => {
+        event.preventDefault();
+        this.props.fetchPlayer(event.target.id);
+        this.props.fetchPlayerStats(event.target.id);
+        return <Redirect to="/player" />
+    }
 
     render () {
         return (
             <div>
                 <p>{this.props.player.name}</p>
-                {this.props.player.roster.roster.map(item => <p>#{item.jerseyNumber} <Link href="">{item.person.fullName}</Link> - {item.position.abbreviation}</p>)}
+                {this.props.player.roster.roster.map(item => <p>#{item.jerseyNumber} <button type="button" onClick={this.handleClick}><Link key={item.person.id} id={item.person.id} to={'/player'}  >{item.person.fullName}</Link> </button>- {item.position.abbreviation}</p>)}
+                
             </div>
         )
     }
 }
 
-export default connect(null)(RosterCard);
+export default connect(null, {fetchPlayer, fetchPlayerStats})(RosterCard);
