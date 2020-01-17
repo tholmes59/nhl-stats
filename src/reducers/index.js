f500400abc8ab2af3ccdaaf58e8d2ba0afee9e7e
Thomas <tholmes59@gmail.com>
2020-01-17 08:40:34 -0800

    add player reducers to riitreducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 
 import teams from './teams';
-import roster from './roster'
+import roster from './roster';
+import player from './player';
 
 const rootReducer = combineReducers({
     teams: teams,
-    roster: roster
+    roster: roster,
+    player: player
 })
 
 export default rootReducer
