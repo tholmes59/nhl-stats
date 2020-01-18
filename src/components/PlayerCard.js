bff1fcc6f492a831f339fb6ec1d3ded18cb33b90
Thomas <tholmes59@gmail.com>
2020-01-18 15:46:51 -0800

    create player card

---
+++ src/components/PlayerCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import { fetchPlayerStats } from '../actions/player';
+import Stats from './Stats'
+
+
+class PlayerCard extends React.Component {
+
+    handleClick = event => {
+        event.preventDefault();
+        this.props.fetchPlayerStats(this.props.player.id)
+    }
+
+    render () {
+        console.log(this.props.stats)
+        return (
+            <div>
+               <p>{this.props.player.fullName} {this.props.player.primaryNumber}</p>
+               <p>{this.props.player.currentTeam.name}</p>
+                <button type="button" onClick={this.handleClick}>See Stats</button>
+                <Stats />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        stats: state.stats
+    }
+}
+
+
+export default connect(mapStateToProps, {fetchPlayerStats})(PlayerCard);
