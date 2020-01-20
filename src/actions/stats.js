0a470f379c5d879673a7f5ac2d30b2297e69ac38
Thomas <tholmes59@gmail.com>
2020-01-20 13:00:25 -0800

    create stats component

---
+++ src/actions/stats.js
@@ -0,0 +1,22 @@
+export const listStats = stats => {
+    return {
+        type: 'LIST_STATS',
+        stats
+    }
+}
+
+
+export const fetchPlayerStats = id => {
+    return (dispatch) => {
+        return fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=yearByYear`)
+        .then(r => r.json())
+        .then(stats => {
+            console.log(stats)
+            if (stats.error) {
+                alert(stats.error)
+            } else {
+                dispatch(listStats(stats))
+            }
+        })
+    }
+}
