cc632e721d9c7234497d4a15da9dc0a2964bba47
Thomas <tholmes59@gmail.com>
2020-01-20 13:02:05 -0800

    add stats reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux'
 import teams from './teams';
 import roster from './roster';
 import player from './player';
+import stats from './stats';
 
 const rootReducer = combineReducers({
     teams: teams,
     roster: roster,
-    player: player
+    player: player,
+    stats: stats
 })
 
 export default rootReducer
