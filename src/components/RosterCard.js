3ee777ea0a44a489fe461da2e0dec4c162a5177d
Thomas <tholmes59@gmail.com>
2020-01-24 09:42:42 -0800

    edit roster card to call player stats async action

--- src/components/RosterCard.js
+++ src/components/RosterCard.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchPlayer } from '../actions/player'
-import { fetchPlayerStats } from '../actions/player'
-import { Redirect } from 'react-router-dom';
+import { fetchPlayerStats } from '../actions/stats'
 
 class RosterCard extends React.Component {
 
@@ -11,14 +10,13 @@ class RosterCard extends React.Component {
         event.preventDefault();
         this.props.fetchPlayer(event.target.id);
         this.props.fetchPlayerStats(event.target.id);
-        return <Redirect to="/player" />
     }
 
     render () {
         return (
             <div>
                 <p>{this.props.player.name}</p>
-                {this.props.player.roster.roster.map(item => <p>#{item.jerseyNumber} <button type="button" onClick={this.handleClick}><Link key={item.person.id} id={item.person.id} to={'/player'}  >{item.person.fullName}</Link> </button>- {item.position.abbreviation}</p>)}
+                {this.props.player.roster.roster.map((item, id) => <p key={id}>#{item.jerseyNumber} <button type="button" onClick={this.handleClick}><Link key={item.person.id} id={item.person.id} to={'/player'}  >{item.person.fullName}</Link> </button>- {item.position.abbreviation}</p>)}
                 
             </div>
         )
