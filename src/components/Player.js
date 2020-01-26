3484dce42d5963c710acbee47da0229e0213fc61
Thomas <tholmes59@gmail.com>
2020-01-26 09:58:15 -0800

    add assists to player component table

--- src/components/Player.js
+++ src/components/Player.js
@@ -13,11 +13,13 @@ const Player = (props) => {
                     <th>Season</th>
                     <th>Team</th>
                     <th>Goals</th>
+                    <th>Assists</th>
                 </tr>
                 <tr>
                     <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.season}</p>))}</td>
                     <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.team.name}</p>))}</td>
                     <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.goals}</p>))}</td>
+                    <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.assists}</p>))}</td>
                 </tr>
             </table>
             
