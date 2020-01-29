30a019450fc33dadb1ac3a3167efe0dec8e3090f
Thomas <tholmes59@gmail.com>
2020-01-28 17:47:31 -0800

    add points to player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -14,12 +14,14 @@ const Player = (props) => {
                     <th>Team</th>
                     <th>Goals</th>
                     <th>Assists</th>
+                    <th>Points</th>
                 </tr>
                 <tr>
                     <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.season}</p>))}</td>
                     <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.team.name}</p>))}</td>
                     <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.goals}</p>))}</td>
                     <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.assists}</p>))}</td>
+                    <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.points}</p>))}</td>
                 </tr>
             </table>
             
