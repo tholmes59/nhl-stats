c48dfe0bedcd9c73f21ba17c8976892ab8075960
Thomas <tholmes59@gmail.com>
2020-02-10 10:48:07 -0800

    add bootstrap CDN link to index.html

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/index.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
