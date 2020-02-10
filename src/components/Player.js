9814ebbd48e9d505d6022afb20e34656d1dd49c0
Thomas <tholmes59@gmail.com>
2020-02-10 10:49:18 -0800

    add table styling to player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Table from 'react-bootstrap/Table';
 
 
 const Player = (props) => {
@@ -10,24 +11,28 @@ const Player = (props) => {
             <p>{props.player.people && props.player.people.map(name => name.currentTeam.name)}</p>
             <p>{props.player.people && props.player.people.map(ht => ht.height)}   {props.player.people && props.player.people.map(wt => wt.weight)}lbs</p>
             <p>DOB: {props.player.people && props.player.people.map(bd => bd.birthDate)}  Age: {props.player.people && props.player.people.map(age => age.currentAge)}</p>
-            <table>
-                <tr>
-                    <th>Season</th>
-                    <th>Team</th>
-                    <th>Games</th>
-                    <th>Goals</th>
-                    <th>Assists</th>
-                    <th>Points</th>
-                </tr>
-                <tr>
-                    <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.season}</p>))}</td>
-                    <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.team.name}</p>))}</td>
-                    <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.games}</p>))}</td>
-                    <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.goals}</p>))}</td>
-                    <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.assists}</p>))}</td>
-                    <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.points}</p>))}</td>
-                </tr>
-            </table>
+            <Table bordered responsive >
+                <thead>
+                    <tr>
+                        <th>Season</th>
+                        <th>Team</th>
+                        <th>Games</th>
+                        <th>Goals</th>
+                        <th>Assists</th>
+                        <th>Points</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.season}</p>))}</td>
+                        <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.team.name}</p>))}</td>
+                        <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.games}</p>))}</td>
+                        <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.goals}</p>))}</td>
+                        <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.assists}</p>))}</td>
+                        <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.points}</p>))}</td>
+                    </tr>
+                </tbody>
+            </Table>
             
         </div>
     )
