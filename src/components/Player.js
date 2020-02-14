f5ca942efe0dc345fc6437b2b8b1bb7b14a6eba5
Thomas <tholmes59@gmail.com>
2020-02-14 07:23:47 -0800

    update layout of table for player stats

--- src/components/Player.js
+++ src/components/Player.js
@@ -23,14 +23,23 @@ const Player = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
+                    {/* <tr>
                         <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.season}</p>))}</td>
                         <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.team.name}</p>))}</td>
                         <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.games}</p>))}</td>
                         <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.goals}</p>))}</td>
                         <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.assists}</p>))}</td>
                         <td>{props.stats.stats && props.stats.stats.map(val => val.splits.map(yr => <p>{yr.stat.points}</p>))}</td>
-                    </tr>
+                    </tr> */}
+                    {props.stats.stats && props.stats.stats.map(val => (val.splits.map(yr => (<tr>
+                        <td>{yr.season}</td>
+                        <td>{yr.team.name}</td>
+                        <td>{yr.stat.games}</td>
+                        <td>{yr.stat.goals}</td>
+                        <td>{yr.stat.assists}</td>
+                        <td>{yr.stat.points}</td>
+                        </tr>))))
+                    }
                 </tbody>
             </Table>
             
