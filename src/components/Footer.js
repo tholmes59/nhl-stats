7966c7e07b8a649cff4a931277c5ac7b30369df6
Thomas <tholmes59@gmail.com>
2020-02-28 07:08:13 -0800

    edit styling for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Footer = () => {
 
+    const footerStyle = {
+        position: 'float',
+        left: '0',
+        bottom: '0',
+        width: '100%',
+        backgroundColor: 'black',
+        color: 'white',
+        textAlign: 'center',
+        // height: '-40px',
+        marginTop: '5px'
+    }
     return (
-        <div>
+        <div style={footerStyle}>
             <p>Footer</p>
         </div>
     )
