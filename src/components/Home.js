e7b0dc48ee8e97c6af94f7ec2eae0af3224daac2
Thomas <tholmes59@gmail.com>
2020-03-14 14:44:35 -0700

    create home component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Home = () => {
+
+    return (
+        <div>
+           <h1>Welcome to NHL Stats!</h1> 
+            <button type="button"><Link to={'/teams'}></Link></button>
+        </div>
+    ) 
+}
+
+export default Home;
