71894c619f754f057d480f665fb34ca5812ab95a
Thomas <tholmes59@gmail.com>
2020-03-26 10:55:10 -0700

    edit styling to heading and button to display over image

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,35 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
 
+    const homeContainer = {
+        position: 'relative',
+        width: '100%'
+    }
+
     const img = {
-        
+        minHeight: '100vh',
+        // width: '100%',
+        // height: 'auto'
+    }
+
+    const heading = {
+        position: 'absolute',
+        top: '70%',
+        left: '20%',
+        color: 'white'
+    }
+
+    const btn = {
+        position: 'absolute',
+        top: '80%',
+        left: '20%'
     }
 
     return (
-        <div>
+        <div style={homeContainer}>
             <img src={require("../assets/David Pastrnak.jpg")} alt="NHL Logo" style={img}/>
-            <h1>Welcome to NHL Stats!</h1> 
-            <button type="button"><Link to={'/teams'}>Teams</Link></button>
+            <h1 style={heading}>Welcome to NHL Stats!</h1> 
+            <button type="button" style={btn}><Link to={'/teams'}>Teams</Link></button>
         </div>
     ) 
 }
