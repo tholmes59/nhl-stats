10572649676b31f80b8218b26da88461de38144d
Thomas <tholmes59@gmail.com>
2020-03-26 10:26:59 -0700

    add image to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,14 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
 
+    const img = {
+        
+    }
+
     return (
         <div>
-           <h1>Welcome to NHL Stats!</h1> 
+            <img src={require("../assets/David Pastrnak.jpg")} alt="NHL Logo" style={img}/>
+            <h1>Welcome to NHL Stats!</h1> 
             <button type="button"><Link to={'/teams'}>Teams</Link></button>
         </div>
     ) 
