9adc553a9663cea02b1e6c18ee9ab269b7a04c99
Thomas <tholmes59@gmail.com>
2020-03-26 14:25:11 -0700

    edit positioning of description text

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,35 +6,44 @@ const Home = () => {
 
     const homeContainer = {
         position: 'relative',
-        // maxWidth: '100%'
-        // backgroundSize: 'cover'
     }
 
     const img = {
         minHeight: '100vh',
         objectFit: 'cover',
         maxWidth: '100%',
-        // height: '100vh'
+    }
+
+    const contentContainer = {
+        position: 'absolute',
+        top: '10%',
+        left: '70%',
+        margin: '0.5rem',
+        padding: '0.5rem'
     }
 
     const heading = {
-        position: 'absolute',
-        top: '40%',
-        left: '80%',
+        color: 'white'
+    }
+
+    const des = {
         color: 'white'
     }
 
     const btn = {
-        position: 'absolute',
-        top: '50%',
-        left: '80%'
+        // position: 'absolute',
+        // top: '50%',
+        // left: '80%'
     }
 
     return (
         <div style={homeContainer}>
             <img src={require("../assets/David Pastrnak.jpg")} alt="NHL Logo" style={img}/>
-            <h1 style={heading}>Welcome to NHL Stats!</h1> 
-            <button type="button" style={btn}><Link to={'/teams'}>Teams</Link></button>
+            <div style={contentContainer}>
+                <h2 style={heading}>Welcome to NHL Stats!</h2> 
+                <p style={des}>View your favorite teams roster and see the stats of all your favorite players</p>
+                <button type="button" style={btn}><Link to={'/teams'}> View Teams</Link></button>
+            </div>
         </div>
     ) 
 }
