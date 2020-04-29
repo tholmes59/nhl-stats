a1df70f6bf5adf6bac005e7da2238446b8d636d6
Thomas <tholmes59@gmail.com>
2020-04-29 09:15:14 -0700

    clean up code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
+  const footerStyle = {
+    position: "absolute",
+    left: "0",
+    bottom: "0",
+    width: "100%",
+    backgroundColor: "black",
+    color: "white",
+    textAlign: "center",
+    height: "3rem",
+    marginTop: "5px",
+    fontSize: "1vmin",
+    paddingTop: ".5rem"
+  };
 
-    const footerStyle = {
-        position: 'absolute',
-        left: '0',
-        bottom: '0',
-        width: '100%',
-        backgroundColor: 'black',
-        color: 'white',
-        textAlign: 'center',
-        height: '3rem',
-        marginTop: '5px',
-        fontSize: '1vmin',
-        paddingTop: '.5rem'
-    }
+  const getYear = () => {
+    return new Date().getFullYear();
+  };
+  return (
+    <div style={footerStyle}>
+      <span>
+        {getYear()} NHL Stats by Tom Holmes
+        <br />
+        "NHL and the NHL Shield are registered trademarks of the National Hockey
+        League. NHL and NHL team marks are the property of the NHL and its
+        teams. © NHL 2020. All Rights Reserved."
+      </span>
+    </div>
+  );
+};
 
-    const getYear = () => {
-        return new Date().getFullYear()
-    }
-    return (
-        <div style={footerStyle}>
-            <span>
-                {getYear()} NHL Stats by Tom Holmes<br/>
-                "NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams. © NHL 2020. All Rights Reserved."
-            </span>
-        </div>
-    )
-}
-
-export default Footer
+export default Footer;
