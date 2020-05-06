93a7f855860ee68ce941cc63e5cd122f7a60c090
Thomas <tholmes59@gmail.com>
2020-05-06 07:03:21 -0700

    clean up code formatting

--- src/components/RosterCard.js
+++ src/components/RosterCard.js
@@ -1,124 +1,152 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { fetchPlayer } from '../actions/player'
-import { fetchPlayerStats } from '../actions/stats'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchPlayer } from "../actions/player";
+import { fetchPlayerStats } from "../actions/stats";
 
 class RosterCard extends React.Component {
+  handleClick = event => {
+    event.preventDefault();
+    this.props.fetchPlayer(event.target.id);
+    this.props.fetchPlayerStats(event.target.id);
+  };
 
-    handleClick = event => {
-        event.preventDefault();
-        this.props.fetchPlayer(event.target.id);
-        this.props.fetchPlayerStats(event.target.id);
-    }
-   
-    render () {
+  render() {
+    const headingStyle = {
+      margin: "1rem",
+      paddingTop: ".5rem",
+      paddingBottom: ".5rem"
+    };
 
-        const headingStyle = {
-            margin: '1rem',
-            paddingTop: '.5rem',
-            paddingBottom: '.5rem'
-        }
+    const hrStyle = {
+      height: "12px",
+      border: "0",
+      boxShadow: "inset 0 12px 12px -12px rgba(0, 0, 0, 0.5)"
+    };
 
-        const hrStyle = {
-            height: '12px',
-            border: '0',
-            boxShadow: 'inset 0 12px 12px -12px rgba(0, 0, 0, 0.5)'
-        }
+    const rosterProfileStyle = {
+      fontFamily: "Sintony",
+      fontSize: "2vw",
+      margin: "1rem 1rem 2rem 1rem",
+      border: "1px solid",
+      width: "fit-content",
+      padding: "1rem",
+      borderRadius: "5px",
+      boxShadow: "5px 10px 18px #888888"
+    };
 
-        const rosterProfileStyle = {
-            fontFamily: 'Sintony',
-            fontSize: '2vw',
-            margin: '1rem 1rem 2rem 1rem',
-            border: '1px solid',
-            width: 'fit-content',
-            padding: '1rem',
-            borderRadius: '5px',
-            boxShadow: '5px 10px 18px #888888'
-        }
+    const headingTeam = {
+      fontFamily: "Sintony",
+      fontSize: "2vw",
+      margin: "1rem"
+      // fontStyle: 'oblique'
+    };
 
-        const headingTeam = {
-            fontFamily: 'Sintony',
-            fontSize: '2vw',
-            margin: '1rem',
-            // fontStyle: 'oblique'
-        }
+    const teamButton = {
+      display: "flex",
+      color: "#FFFFFF",
+      backgroundColor: "#F86D02",
+      padding: ".5vw 1vh",
+      borderRadius: "5px",
+      border: "solid #404040 .5px",
+      width: "fit-content",
+      fontSize: "1vw"
+    };
 
-        const teamButton = {
-            display: 'flex',
-            color: '#FFFFFF',
-            backgroundColor: '#F86D02',
-            padding: '.5vw 1vh',
-            borderRadius: '5px',
-            border: 'solid #404040 .5px',
-            width: 'fit-content',
-            fontSize: '1vw'
-        }
-        const playerStyle = {
-            display: 'grid',
-            gridTemplateColumns: 'repeat(auto-fit, minmax(15rem, 1fr))',
-            gridGap: '.5rem',
-            fontFamily: 'Sintony',
-            // fontSize: '2.5vmin',
-            margin: '1rem'
-        }
-        const playerButton = {
-            border: 'none',
-            background: 'none',
-            textDecoration: 'underline',
+    const playerStyle = {
+      display: "grid",
+      gridTemplateColumns: "repeat(auto-fit, minmax(15rem, 1fr))",
+      gridGap: ".5rem",
+      fontFamily: "Sintony",
+      // fontSize: '2.5vmin',
+      margin: "1rem"
+    };
 
-        }
+    const playerButton = {
+      border: "none",
+      background: "none",
+      textDecoration: "underline"
+    };
 
-        const playerBox = {
-            // width: 'fit-content',
-            // padding: '.3rem 1rem',
-            // border: '1px solid',
-            // margin: '1rem 0',
-            // borderRadius: '5px',
-            // boxShadow: '5px 10px 18px #888888'
-            padding: '.5rem 0',
-            fontWeight: 'bold'
-        }
-        const playerLink = {
-            // textDecoration: 'none',
-            color: 'black',
-            fontWeight: 'bold'
-        }
-
-        const  viewTeamSite = () => {
-            window.open(this.props.player.officialSiteUrl)
-        }
+    const playerBox = {
+      // width: 'fit-content',
+      // padding: '.3rem 1rem',
+      // border: '1px solid',
+      // margin: '1rem 0',
+      // borderRadius: '5px',
+      // boxShadow: '5px 10px 18px #888888'
+      padding: ".5rem 0",
+      fontWeight: "bold"
+    };
     
-        return (
-            <div>
-                <div style={headingStyle}>
-                    <h1>{this.props.player.teamName} Roster</h1>
-                    <hr style={hrStyle}/>
-                </div>
-                <div style={rosterProfileStyle}>
-                    <p className="team-style">{this.props.player.name}</p>
-                    <p className="team-style">{this.props.player.conference.name} Conference</p>
-                    <p className="team-style">{this.props.player.division.name} Division</p>
-                    <p className="team-style">{this.props.player.venue.name} | {this.props.player.venue.city}</p>
-                    <p className="team-style">First year {this.props.player.firstYearOfPlay}</p>
-                    <button onClick={viewTeamSite} style={teamButton}><span className="team-button-style">Official {this.props.player.teamName} Site</span></button>
-                </div>
-                <hr style={hrStyle}/>
-                <p style={headingTeam}><span className="player-style">Players:</span></p>
-                <hr/>
-                <div style={playerStyle}>
-                    {this.props.player.roster.roster.map((item, id) => 
-                    <div style={playerBox} className="roster-button">
-                        <span key={id}>#{item.jerseyNumber} |
-                        <button type="button" style={playerButton} onClick={this.handleClick}><Link style={playerLink} key={item.person.id} id={item.person.id} to={'/player'}  >{item.person.fullName}</Link> </button>
-                        | {item.position.abbreviation}</span>
-                    </div>
-                        )}
-                </div>
+    const playerLink = {
+      // textDecoration: 'none',
+      color: "black",
+      fontWeight: "bold"
+    };
+
+    const viewTeamSite = () => {
+      window.open(this.props.player.officialSiteUrl);
+    };
+
+    return (
+      <div>
+        <div style={headingStyle}>
+          <h1>{this.props.player.teamName} Roster</h1>
+          <hr style={hrStyle} />
+        </div>
+        <div style={rosterProfileStyle}>
+          <p className="team-style">{this.props.player.name}</p>
+          <p className="team-style">
+            {this.props.player.conference.name} Conference
+          </p>
+          <p className="team-style">
+            {this.props.player.division.name} Division
+          </p>
+          <p className="team-style">
+            {this.props.player.venue.name} | {this.props.player.venue.city}
+          </p>
+          <p className="team-style">
+            First year {this.props.player.firstYearOfPlay}
+          </p>
+          <button onClick={viewTeamSite} style={teamButton}>
+            <span className="team-button-style">
+              Official {this.props.player.teamName} Site
+            </span>
+          </button>
+        </div>
+        <hr style={hrStyle} />
+        <p style={headingTeam}>
+          <span className="player-style">Players:</span>
+        </p>
+        <hr />
+        <div style={playerStyle}>
+          {this.props.player.roster.roster.map((item, id) => (
+            <div style={playerBox} className="roster-button">
+              <span key={id}>
+                #{item.jerseyNumber} |
+                <button
+                  type="button"
+                  style={playerButton}
+                  onClick={this.handleClick}
+                >
+                  <Link
+                    style={playerLink}
+                    key={item.person.id}
+                    id={item.person.id}
+                    to={"/player"}
+                  >
+                    {item.person.fullName}
+                  </Link>{" "}
+                </button>
+                | {item.position.abbreviation}
+              </span>
             </div>
-        )
-    }
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default connect(null, {fetchPlayer, fetchPlayerStats})(RosterCard);
