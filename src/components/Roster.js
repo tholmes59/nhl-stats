7919285471a76ecadec00b12a7df5ae3d8ec15b8
Thomas <tholmes59@gmail.com>
2020-05-11 10:28:08 -0700

    clean up code

--- src/components/Roster.js
+++ src/components/Roster.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import RosterCard from './RosterCard'
+import React from "react";
+import { connect } from "react-redux";
+import RosterCard from "./RosterCard";
 
-const Roster = (props) => {
-    const rosterCards = props.roster.teams && props.roster.teams.map(player => <RosterCard key={player.id} player={player} />)
-    const rosterStyle = {
-        paddingBottom: '3rem'
-    }
-    return (
-        <div style={rosterStyle}>
-            {rosterCards}
-        </div>
-    )
-}
+const Roster = props => {
+  const rosterCards =
+    props.roster.teams &&
+    props.roster.teams.map(player => (
+      <RosterCard key={player.id} player={player} />
+    ));
+
+  const rosterStyle = {
+    paddingBottom: "3rem"
+  };
+  
+  return <div style={rosterStyle}>{rosterCards}</div>;
+};
 
 const mapStateToProps = state => {
-    return {
-        roster: state.roster
-    }
-}
+  return {
+    roster: state.roster
+  };
+};
 
-export default connect(mapStateToProps)(Roster)
+export default connect(mapStateToProps)(Roster);
