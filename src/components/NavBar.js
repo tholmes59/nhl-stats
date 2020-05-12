1fc67f3a0258b43b87f66ce5a27ddbcc0f3eda7a
Thomas <tholmes59@gmail.com>
2020-05-12 11:37:35 -0700

    clean up code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,30 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  // return (
-  //     <Navbar bg="dark" variant="dark">
-  //         <Navbar.Brand href="/">
-  //             <img
-  //                 src={require("../assets/nhl.jpg")}
-  //                 width="50"
-  //                 height="50"
-  //                 className="d-inline-block align-top"
-  //                 alt="NHL logo"
-  //             />
-  //         </Navbar.Brand>
-  //             <Nav className="ml-auto">
-  //                 <Nav.Link href="/">Home</Nav.Link>
-  //             </Nav>
-  //     </Navbar>
-  // )
   const navBar = {
     maxWidth: "100%",
     backgroundColor: "black",
     paddingLeft: "20px",
     paddingRight: "10px",
     overflow: "hidden",
-    // paddingTop: '1px',
-    // paddingBottom: '1px',
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center"
@@ -33,13 +15,7 @@ const NavBar = () => {
 
   const logo = {
     display: "inline-block",
-    // width: '175px',
     verticalAlign: "middle"
-    // color: 'white',
-    // paddingTop: '5px',
-    // objectFit: 'cover',
-    // minHeight: '100vh'
-    // height: '100%'
   };
 
   const img = {
