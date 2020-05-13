024829def0d6832c28dbfa0c7dc7306210d55d03
Thomas <tholmes59@gmail.com>
2020-05-13 13:24:23 -0700

    clean up code

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import TeamsCard from './TeamsCard'
+import React from "react";
+import { connect } from "react-redux";
+import TeamsCard from "./TeamsCard";
 
-const Teams = (props) => {
+const Teams = props => {
+  const pageLayout = {
+    paddingBottom: "3rem"
+  };
 
-    const pageLayout = {
-        // margin: '10px',
-        paddingBottom: '3rem'
-    }
-    const gridLayout = {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fit, minmax(20rem, 1fr))',
-        gridGap: '0.625rem',
-        margin: '0.625rem 0.625rem'
-    }
-    const teamCards = props.teams.teams && props.teams.teams.map(team => <TeamsCard key={team.id} team={team} />)
-    return (
-        <div style={pageLayout}>
-            <div style={gridLayout}>
-                {teamCards}
-            </div>
-        </div>
-    )
-}
+  const gridLayout = {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(20rem, 1fr))",
+    gridGap: "0.625rem",
+    margin: "0.625rem 0.625rem"
+  };
+  
+  const teamCards =
+    props.teams.teams &&
+    props.teams.teams.map(team => <TeamsCard key={team.id} team={team} />);
+  return (
+    <div style={pageLayout}>
+      <div style={gridLayout}>{teamCards}</div>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
-    return {
-        teams: state.teams
-    }
-}
+  return {
+    teams: state.teams
+  };
+};
 
-export default connect(mapStateToProps)(Teams)
+export default connect(mapStateToProps)(Teams);
