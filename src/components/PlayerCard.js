53bd5b697b4f4e3cde059bf26b179b7f45e4fd11
Thomas <tholmes59@gmail.com>
2020-05-21 09:57:45 -0700

    clean up code

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,36 +1,33 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import { fetchPlayerStats } from '../actions/stats';
-import Stats from './Stats'
-
+import React from "react";
+import { connect } from "react-redux";
+import { fetchPlayerStats } from "../actions/stats";
 
 class PlayerCard extends React.Component {
+  handleClick = (event) => {
+    event.preventDefault();
+    this.props.fetchPlayerStats(this.props.player.id);
+  };
 
-    handleClick = event => {
-        event.preventDefault();
-        this.props.fetchPlayerStats(this.props.player.id)
-    }
-
-    render () {
-        console.log(this.props.player)
-        return (
-            <div>
-               <p>{this.props.player.fullName} {this.props.player.primaryNumber}</p>
-               <p>{this.props.player.currentTeam.name}</p>
-               {/* <p>{this.props.stats.stats && this.props.stats.stats.type.displayName}</p> */}
-                <button type="button" onClick={this.handleClick}>See Stats</button>
-                
-                {/* <Stats props={this.props.stats}/> */}
-            </div>
-        )
-    }
+  render() {
+    console.log(this.props.player);
+    return (
+      <div>
+        <p>
+          {this.props.player.fullName} {this.props.player.primaryNumber}
+        </p>
+        <p>{this.props.player.currentTeam.name}</p>
+        <button type="button" onClick={this.handleClick}>
+          See Stats
+        </button>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        stats: state.stats
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    stats: state.stats,
+  };
+};
 
-
-export default connect(mapStateToProps, {fetchPlayerStats})(PlayerCard);
+export default connect(mapStateToProps, { fetchPlayerStats })(PlayerCard);
