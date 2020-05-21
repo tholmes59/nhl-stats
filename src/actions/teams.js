4a3646b666a9085b7927f55db0082de5df895cdf
Thomas <tholmes59@gmail.com>
2020-05-21 09:50:00 -0700

    clean up code

--- src/actions/teams.js
+++ src/actions/teams.js
@@ -1,22 +1,21 @@
-export const listTeams = teams => {
-    return {
-        type: 'LIST_TEAMS',
-        teams
-    }
-}
-
+export const listTeams = (teams) => {
+  return {
+    type: "LIST_TEAMS",
+    teams,
+  };
+};
 
 export const fetchTeams = () => {
-    return (dispatch) => {
-        return fetch(`https://statsapi.web.nhl.com/api/v1/teams`)
-        .then(r => r.json())
-        .then(teams => {
-            console.log(teams)
-            if (teams.error) {
-                alert(teams.error)
-            } else {
-                dispatch(listTeams(teams))
-            }
-        })
-    }
-}
+  return (dispatch) => {
+    return fetch(`https://statsapi.web.nhl.com/api/v1/teams`)
+      .then((r) => r.json())
+      .then((teams) => {
+        console.log(teams);
+        if (teams.error) {
+          alert(teams.error);
+        } else {
+          dispatch(listTeams(teams));
+        }
+      });
+  };
+};
