3757e8fc7038e68bdca03aea684f9a5b4b2c5e36
Thomas <tholmes59@gmail.com>
2020-05-21 09:49:24 -0700

    clean up code

--- src/actions/roster.js
+++ src/actions/roster.js
@@ -1,22 +1,23 @@
-export const listRoster = roster => {
-    return {
-        type: 'LIST_ROSTER',
-        roster
-    }
-}
+export const listRoster = (roster) => {
+  return {
+    type: "LIST_ROSTER",
+    roster,
+  };
+};
 
-
-export const fetchRoster = id => {
-    return (dispatch) => {
-        return fetch(`https://statsapi.web.nhl.com/api/v1/teams/${id}?expand=team.roster`)
-        .then(r => r.json())
-        .then(roster => {
-            console.log(roster)
-            if (roster.error) {
-                alert(roster.error)
-            } else {
-                dispatch(listRoster(roster))
-            }
-        })
-    }
-}
+export const fetchRoster = (id) => {
+  return (dispatch) => {
+    return fetch(
+      `https://statsapi.web.nhl.com/api/v1/teams/${id}?expand=team.roster`
+    )
+      .then((r) => r.json())
+      .then((roster) => {
+        console.log(roster);
+        if (roster.error) {
+          alert(roster.error);
+        } else {
+          dispatch(listRoster(roster));
+        }
+      });
+  };
+};
