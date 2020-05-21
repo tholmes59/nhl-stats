49ef8ef2a9061c9f55cf8be15f30bb4a8384af79
Thomas <tholmes59@gmail.com>
2020-05-21 09:49:03 -0700

    clean up code

--- src/actions/player.js
+++ src/actions/player.js
@@ -1,23 +1,21 @@
-export const listPlayer = player => {
-    return {
-        type: 'LIST_PLAYER',
-        player
-    }
-}
+export const listPlayer = (player) => {
+  return {
+    type: "LIST_PLAYER",
+    player,
+  };
+};
 
-
-export const fetchPlayer = id => {
-    return (dispatch) => {
-        return fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}`)
-        .then(r => r.json())
-        .then(player => {
-            console.log(player)
-            if (player.error) {
-                alert(player.error)
-            } else {
-                dispatch(listPlayer(player))
-            }
-        })
-    }
-}
-
+export const fetchPlayer = (id) => {
+  return (dispatch) => {
+    return fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}`)
+      .then((r) => r.json())
+      .then((player) => {
+        console.log(player);
+        if (player.error) {
+          alert(player.error);
+        } else {
+          dispatch(listPlayer(player));
+        }
+      });
+  };
+};
